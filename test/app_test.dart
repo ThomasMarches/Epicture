01a66994dcbed34fd2cfd99f32860374670ce233
Thomas Marches <56929277+ThomasMarches@users.noreply.github.com>
2021-08-23 23:04:15 +0200

    refactor: remove useless test

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,12 +1,6 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:epicture/core/presentation/app/app.dart';
-import 'package:epicture/core/presentation/pages/home_page.dart';
+// import 'package:flutter_test/flutter_test.dart';
+// import 'package:epicture/core/presentation/app/app.dart';
+// import 'package:epicture/core/presentation/pages/home_page.dart';
 
 void main() {
-  group('App', () {
-    testWidgets('renders HomePage', (tester) async {
-      await tester.pumpWidget(const App());
-      expect(find.byType(HomePage), findsOneWidget);
-    });
-  });
 }
