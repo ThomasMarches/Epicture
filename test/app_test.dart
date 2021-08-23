c7bddfe14f4b835501ab5cf39372b95f68722a44
Thomas Marches <56929277+ThomasMarches@users.noreply.github.com>
2021-08-23 23:11:18 +0200

    refactor:  tests

--- test/app_test.dart
+++ test/app_test.dart
@@ -1,5 +1,12 @@
-// import 'package:flutter_test/flutter_test.dart';
-// import 'package:epicture/core/presentation/app/app.dart';
-// import 'package:epicture/core/presentation/pages/home_page.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:very_good_starter/core/presentation/app/app.dart';
+import 'package:very_good_starter/core/presentation/pages/home_page.dart';
 
-void main() {}
+void main() {
+  group('App', () {
+    testWidgets('renders HomePage', (tester) async {
+      await tester.pumpWidget(const App());
+      expect(find.byType(HomePage), findsOneWidget);
+    });
+  });
+}
