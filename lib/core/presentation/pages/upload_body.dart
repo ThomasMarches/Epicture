e976dc9f8084914f8ada6cb95338bbf626f0eeec
Thomas Marches <56929277+ThomasMarches@users.noreply.github.com>
2021-09-23 02:18:55 +0200

    feat: add take pictures from gallery and camera

--- lib/core/presentation/pages/upload_body.dart
+++ lib/core/presentation/pages/upload_body.dart
@@ -12,61 +12,146 @@ class UploadBody extends StatefulWidget {
 
 class _UploadBodyState extends State<UploadBody> {
   final _picker = ImagePicker();
-  Image? previewImage;
+  File? previewImage;
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return previewImage == null
+        ? Center(
+            child: Column(
+              children: [
+                const Spacer(flex: 2),
+                IconButton(
+                  onPressed: () async {
+                    final xFile =
+                        await _picker.pickImage(source: ImageSource.gallery);
+                    if (xFile != null) {
+                      setState(() {
+                        previewImage = File(xFile.path);
+                      });
+                    }
+                  },
+                  icon: const Icon(
+                    Icons.image,
+                    color: Colors.black,
+                    size: 40,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Press this button to search a picture from your gallery.',
+                  ),
+                ),
+                const Spacer(flex: 1),
+                IconButton(
+                  onPressed: () async {
+                    final xFile =
+                        await _picker.pickImage(source: ImageSource.camera);
+                    if (xFile != null) {
+                      setState(() {
+                        previewImage = File(xFile.path);
+                      });
+                    }
+                  },
+                  icon: const Icon(
+                    Icons.add_a_photo_outlined,
+                    color: Colors.black,
+                    size: 40,
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Text(
+                    'Press this button to take a picture with your phone.',
+                  ),
+                ),
+                const Spacer(flex: 2),
+              ],
+            ),
+          )
+        : ImagePreview(previewImage: previewImage);
+  }
+}
+
+class ImagePreview extends StatefulWidget {
+  const ImagePreview({
+    Key? key,
+    required this.previewImage,
+  }) : super(key: key);
+
+  final File? previewImage;
+
+  @override
+  State<ImagePreview> createState() => _ImagePreviewState();
+}
+
+class _ImagePreviewState extends State<ImagePreview> {
+  String? imageTitle;
+  String? imageDescription;
+
+  @override
+  Widget build(BuildContext context) {
+    return Flexible(
       child: Column(
-        children: [
-          const Spacer(flex: 2),
-          IconButton(
-            onPressed: () async {
-              final xFile =
-                  await _picker.pickImage(source: ImageSource.gallery);
-              if (xFile != null) {
-                setState(() {
-                  previewImage = Image.file(File(xFile.path));
-                });
-              }
-            },
-            icon: const Icon(
-              Icons.image,
-              color: Colors.black,
-              size: 40,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.file(
+              widget.previewImage!,
+              width: 300,
+              height: 300,
+              fit: BoxFit.fill,
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text(
-              'Press this button to search a picture from your gallery.',
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Spacer(),
+                IconButton(
+                  onPressed: () async {},
+                  icon: const Icon(
+                    Icons.delete,
+                    color: Colors.grey,
+                    size: 40,
+                  ),
+                ),
+                const Spacer(),
+                IconButton(
+                  onPressed: () async {},
+                  icon: const Icon(
+                    Icons.upload,
+                    color: Colors.grey,
+                    size: 40,
+                  ),
+                ),
+                const Spacer(),
+              ],
             ),
-          ),
-          const Spacer(flex: 1),
-          IconButton(
-            onPressed: () async {
-              final xFile = await _picker.pickImage(source: ImageSource.camera);
-              if (xFile != null) {
-                setState(() {
-                  previewImage = Image.file(File(xFile.path));
-                });
-              }
-            },
-            icon: const Icon(
-              Icons.add_a_photo_outlined,
-              color: Colors.black,
-              size: 40,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onSubmitted: (String input) {
+                  imageTitle = input;
+                },
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Image title',
+                ),
+              ),
             ),
-          ),
-          const Padding(
-            padding: EdgeInsets.all(8.0),
-            child: Text(
-              'Press this button to take a picture with your phone.',
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                onSubmitted: (String input) {
+                  imageDescription = input;
+                },
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Image description',
+                ),
+              ),
             ),
-          ),
-          const Spacer(flex: 2),
-        ],
-      ),
+          ],
+        ),
     );
   }
 }
