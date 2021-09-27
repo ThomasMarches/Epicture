0d34ea18e5b6b7d217006371188e4c9336ac4730
Thomas Marches <56929277+ThomasMarches@users.noreply.github.com>
2021-09-27 16:06:23 +0200

    test: add ImgurImage test + complete ImgurComment test

--- test/models/imgur_comments_test.dart
+++ test/models/imgur_comments_test.dart
@@ -5,7 +5,7 @@ import 'package:epicture/core/data/models/imgur_comments.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  group('User Model tests', () {
+  group('Imgur Comments tests', () {
     test('FromMap method test', () {
       final file =
           File('test/ressources/imgur_comments.json').readAsStringSync();
@@ -13,7 +13,16 @@ void main() {
       final comment =
           ImgurComments.fromMap(jsonDecode(file) as Map<String, dynamic>);
 
+      expect(comment.id, 1110);
       expect(comment.comment, 'This is a Test Caption sent via the API!');
+      expect(comment.vote, null);
+      expect(comment.ups, 5);
+      expect(comment.downs, 0);
+      expect(comment.author, 'joshTest');
+      expect(
+        comment.datetime,
+        DateTime.fromMillisecondsSinceEpoch(1346977487 * 1000),
+      );
     });
   });
 }
---
+++ test/models/imgur_images_test.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+import 'dart:io';
+
+import 'package:epicture/core/data/models/imgur_image.dart';
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  group('Imgur Images tests', () {
+    test('FromMap method test', () {
+      final file =
+          File('test/ressources/imgur_images.json').readAsStringSync();
+
+      final comment =
+          ImgurImages.fromMap(jsonDecode(file) as Map<String, dynamic>);
+
+      expect(comment.id, 'SbBGk');
+      expect(comment.title, null);
+      expect(comment.description, null);
+      expect(comment.vote, null);
+      expect(comment.width, 2559);
+      expect(comment.height, 1439);
+      expect(comment.type, 'jpeg');
+      expect(comment.favorite, false);
+      expect(comment.link, 'http://i.imgur.com/SbBGk.jpg');
+      expect(comment.section, null);
+      expect(
+        comment.datetime,
+        DateTime.fromMillisecondsSinceEpoch(1341533193 * 1000),
+      );
+    });
+  });
+}
