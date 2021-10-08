054ac47301e3639b6e9de7665ef0914ab2ccdd18
Thomas Marches <56929277+ThomasMarches@users.noreply.github.com>
2021-10-08 16:41:06 +0200

    refactor: fix image sort inside data source

--- lib/core/data/sources/imgur_data_source.dart
+++ lib/core/data/sources/imgur_data_source.dart
@@ -141,8 +141,8 @@ class ImgurDataSource {
       );
       finalList.removeWhere(
         (element) {
-          return (element.type != 'jpg' && element.type != 'png') ||
-              element.height > 1000;
+          return (element.type != 'jpeg' && element.type != 'png') ||
+              element.height > 600;
         },
       );
       return finalList;
@@ -188,7 +188,7 @@ class ImgurDataSource {
 
       finalList.removeWhere(
         (element) {
-          return (element.type != 'jpg' && element.type != 'png') ||
+          return (element.type != 'jpeg' && element.type != 'png') ||
               element.height > 600;
         },
       );
