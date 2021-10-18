fcdf23041811fcf4e8995da11326932913eb572c
Thomas Marches <56929277+ThomasMarches@users.noreply.github.com>
2021-10-18 11:52:34 +0200

    test: add new SearchBody tests + set minimum coverage to 50%

--- test/widgets/search_body_test.dart
+++ test/widgets/search_body_test.dart
@@ -1,17 +1,39 @@
+import 'package:epicture/core/domain/entities/user_entity.dart';
+import 'package:epicture/core/presentation/bloc/user_bloc/user_bloc.dart';
 import 'package:epicture/core/presentation/pages/search_body.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:network_image_mock/network_image_mock.dart';
 
 void main() {
   group('SearchBody', () {
     testWidgets('renders SearchBody', (tester) async {
-      await tester.pumpWidget(const MaterialApp(
-        home: Scaffold(
-          body: SearchBody(),
-        ),
-      ));
-      expect(find.byType(SearchBody), findsOneWidget);
-      expect(find.byType(TextField), findsOneWidget);
+      mockNetworkImagesFor(() async {
+        final userBloc = UserBloc();
+        userBloc.emit(const UserLoadedState(
+            user: UserEntity(
+          accessToken: '',
+          refreshToken: '',
+          accountUsername: '',
+          accountId: '',
+        )));
+        await tester.pumpWidget(BlocProvider(
+          create: (context) => userBloc,
+          child: const MaterialApp(
+            home: Scaffold(
+              body: SearchBody(),
+            ),
+          ),
+        ));
+      }).then((value) async {
+        expect(find.byType(SearchBody), findsOneWidget);
+        expect(find.byType(TextField), findsOneWidget);
+        await tester.enterText(find.byType(TextField), 'testTag');
+        await tester.testTextInput.receiveAction(TextInputAction.done);
+        await tester.pump();
+        expect(find.text('testTag'), findsOneWidget);
+      });
     });
   });
 }
